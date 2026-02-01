4bf053138461a21e97c89f9846e733e15546ea1c
Ailurus-Feline <myk0629@outlook.com>
2026-02-01 13:56:12 +0800

    Change names of some variables

--- include/vrs/vehicle.hpp
+++ include/vrs/vehicle.hpp
@@ -11,10 +11,10 @@ namespace vrs {
 
             static constexpr std::chrono::days reminder_time {30};
 
-            std::string company_name_;
-            date driver_license_expiry_;
-            date permit_expiry_;
-            date inspection_expiry_;
+            std::string company;
+            date driver_license_expiry;
+            date permit_expiry;
+            date inspection_expiry;
 
             //Returns true if dues within 30 days or has expired.
             static bool is_due_soon(const date &expiry_date) noexcept {
@@ -28,23 +28,23 @@ namespace vrs {
 
         public:
             explicit Vehicle(std::string name, date license, date permit, date inspection) : 
-            company_name_(name),
-            driver_license_expiry_(license),
-            permit_expiry_(permit),
-            inspection_expiry_(inspection)
+            company(name),
+            driver_license_expiry(license),
+            permit_expiry(permit),
+            inspection_expiry(inspection)
             {};
 
             // Returns true if expires within 30 days.
             bool is_license_due() const noexcept {
-                return is_due_soon(driver_license_expiry_);
+                return is_due_soon(driver_license_expiry);
             }
             // Returns true if expires within 30 days.
             bool is_permit_due() const noexcept {
-               return is_due_soon(permit_expiry_);
+               return is_due_soon(permit_expiry);
             } 
             // Returns true if expires within 30 days.
             bool is_inspection_due() const noexcept {
-               return is_due_soon(inspection_expiry_);
+               return is_due_soon(inspection_expiry);
             } 
             ~Vehicle() = default;
     };
